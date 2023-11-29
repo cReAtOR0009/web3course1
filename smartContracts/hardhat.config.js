ca8787e7275e745818c751419e4f2942ec249847
cReAtOR0009 <epidnugoairdrop@gmail.com>
2023-11-29 10:46:01 +0100

    downgraded ethers to be able to deploy contract

---
+++ smartContracts/hardhat.config.js
@@ -0,0 +1,12 @@
+require("@nomiclabs/hardhat-waffle");
+//https://eth-sepolia.g.alchemy.com/v2/QitTIYktgcpuG1qErOUiISIL6NDO9MlD
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.19",
+  networks:{
+    sepolia:{
+      url:"https://eth-sepolia.g.alchemy.com/v2/QitTIYktgcpuG1qErOUiISIL6NDO9MlD",
+      accounts:[ "425fd0fe52c891c026b00773f55255e4cbf4e7a58cc20eea0537da2b48014b0e" ]
+    }
+  }
+};
