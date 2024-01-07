d70f35429d547ae74a07a82694038f40080671a9
cReAtOR0009 <epidnugoairdrop@gmail.com>
2024-01-07 02:30:06 +0100

    fixed nav toggle

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -82,7 +82,7 @@ const Navbar = () => {
             />
             <ul
               className={`z-10 fixed -top-0 -right-2 p-3 w-[70vw] h-screen shadow-2xl md:hidden list-none
-              flex flex-col justify-start items-end rounded-md blue-glassmorphism text-white animate-slide-in ${
+              flex flex-col justify-start items-start rounded-md blue-glassmorphism text-white animate-slide-in ${
                 isDarkTheme ? "dark-theme" : ""
               }`}
             >
